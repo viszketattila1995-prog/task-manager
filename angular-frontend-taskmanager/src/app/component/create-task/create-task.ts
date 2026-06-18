9dd27c0745cc6defdbc4890a893d74987d678e02
Viszket Attila <viszket.attila1995@gmail.com>
2026-06-18 03:27:03 +0200

    FE new task form

--- angular-frontend-taskmanager/src/app/component/create-task/create-task.ts
+++ angular-frontend-taskmanager/src/app/component/create-task/create-task.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {TaskService} from '../../service/task.service';
+import {Router} from '@angular/router';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-create-task',
-  imports: [],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './create-task.html',
   styleUrl: './create-task.css',
 })
-export class CreateTask {}
+export class CreateTask {
+
+  private taskService = inject(TaskService)
+
+  private formBuilder = inject(FormBuilder)
+
+  private router = inject(Router)
+
+  taskForm: FormGroup = this.formBuilder.group({
+    name: ['', Validators.required],
+    description: ['', Validators.required]
+  })
+
+  createNewTask() {
+    this.taskService.createTask(this.taskForm.value).subscribe({
+      next: () => {
+        this.taskForm.reset()
+        this.router.navigate(['/task-list'])
+    }
+    })
+  }
+}
