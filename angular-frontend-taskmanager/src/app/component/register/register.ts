b9f81570df424caae994bbdc8bfe1055d6b4d2f8
Viszket Attila <viszket.attila1995@gmail.com>
2026-06-19 20:19:10 +0200

    Register component done

--- angular-frontend-taskmanager/src/app/component/register/register.ts
+++ angular-frontend-taskmanager/src/app/component/register/register.ts
@@ -1,9 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AuthService} from '../../service/auth.service';
+import {Router, RouterLink} from '@angular/router';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule, NgClass, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class Register {
+
+  private authService = inject(AuthService)
+
+  private formBuilder = inject(FormBuilder)
+
+  private router = inject(Router)
+
+  errorMessage = signal<string | undefined>(undefined);
+
+  registerForm: FormGroup = this.formBuilder.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  register() {
+    this.authService.registerUser(this.registerForm.value).subscribe({
+      next: () => {
+        this.registerForm.reset()
+        this.router.navigate(['/login'])
+      },
+      error: (err) => {
+        if (err.error.fieldErrors) {
+          for (const validationError of err.error.fieldErrors) {
+            const formControl = this.registerForm.get(validationError.field)
+            if (formControl) {
+              formControl.setErrors({serverError: validationError.message})
+            }
+          }
+        } else {
+          this.errorMessage.set(err.error.error)
+        }
+      }
+    })
+  }
+}
